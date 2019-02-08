f1064f6f991637c700042236b10981a6d1cec123
habib1234731 <habibrehman1234731@gmail.com>
2019-02-07 23:29:10 -0500

    implemented the rest of the required functions and ran two tests

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -3,6 +3,8 @@ package com.lambdaschool;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Stack stack = new Stack(1000);
+        System.out.println(stack.getTop());
+//        System.out.println(stack.pop());
     }
 }
--- src/com/lambdaschool/Stack.java
+++ src/com/lambdaschool/Stack.java
@@ -5,9 +5,33 @@ public class Stack {
     private String[] stack;
     private int top;
 
-    public Stack(int size) {
+    public Stack (int size) {
         this.size = size;
         stack = new String[size];
-        this.top = -1;
+        top = -1;
+    }
+
+    // returns the number of elements in the stack
+    public String getTop() {
+        if (top == -1) return "There are no items in your stack";
+        return Integer.toString(top);
+    }
+
+    public void push (String str) {
+        if (stack.length > size) throw new UnsupportedOperationException("Stack limit reached");
+        stack[++top] = str;
+    }
+
+    public String pop () {
+        if (top == -1) throw new UnsupportedOperationException("Stack is empty");
+        return stack[top--];
+    }
+
+    public void print() {
+        System.out.println("[");
+        for (String s : stack) {
+            System.out.print("'" + s + "', ");
+        }
+        System.out.println("]");
     }
 }
